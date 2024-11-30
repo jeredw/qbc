f9c866a96dd11f0ebd7f4ea6e69cb2a700ef8394
Jered Wierzbicki <jeredw@gmail.com>
2024-11-30 11:17:36 -0800

    move to ts

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
-build/QBasicParser.java: src/QBasicParser.g4 src/QBasicLexer.g4
+build/QBasicParser.ts: src/QBasicParser.g4 src/QBasicLexer.g4
 	mkdir -p build
-	(cd src && antlr -Werror QBasicLexer.g4 -o ../build)
-	(cd src && antlr -Werror QBasicParser.g4 -o ../build)
-	(cd build && javac *.java)
+	antlr4ng -Dlanguage=TypeScript -listener -Xexact-output-dir -Werror src/QBasicLexer.g4 -o build
+	antlr4ng -Dlanguage=TypeScript -listener -Xexact-output-dir -Werror src/QBasicParser.g4 -o build
 
 .PHONY: test
-test: build/QBasicParser.java test/*/*.bas
-	python3 src/test_parser.py test/{parser,manual,programs}/*.bas
+test: src/TestParser.ts build/QBasicParser.ts test/*/*.bas
+	deno run --allow-write --allow-read --allow-run src/TestParser.ts test/{parser,manual,programs}/*.bas
 
 .PHONY: clean
 clean:
 	rm -rf build
+
+.PHONY: gettools
+gettools:
+	npm install antlr4ng
+	npm install --save-dev antlr4ng-cli
---
+++ src/TestParser.ts
@@ -0,0 +1,82 @@
+import { CharStream, CommonTokenStream, BaseErrorListener } from "antlr4ng";
+import { QBasicLexer } from "../build/QBasicLexer.js";
+import { QBasicParser } from "../build/QBasicParser.js";
+
+class ThrowingErrorListener extends BaseErrorListener {
+  override syntaxError(recognizer, offendingSymbol, line, column, msg, e) {
+    throw `${line}:${column} ${offendingSymbol}: ${msg}`;
+  }
+}
+
+function parseProgram(text: string) {
+  const inputStream = CharStream.fromString(text);
+  const lexer = new QBasicLexer(inputStream);
+  const tokenStream = new CommonTokenStream(lexer);
+  const parser = new QBasicParser(tokenStream);
+  parser.removeErrorListeners();
+  parser.addErrorListener(new ThrowingErrorListener());
+  const tree = parser.program();
+  return tree.toStringTree(parser);
+}
+
+function prettyPrint(tree: string): string {
+  const output = [];
+  let depth = 0;
+  for (let i = 0; i < tree.length; i++) {
+    const ch = tree.charAt(i);
+    if (ch == '(') {
+      if (depth > 0) {
+        output.push('\n');
+        output.push(' '.repeat(depth * 2));
+      }
+      depth += 1;
+    } else if (ch == ')') {
+      depth -= 1;
+    }
+    output.push(ch);
+  }
+  output.push('\n');
+  return output.join('');
+}
+
+async function runTests(tests: string[]) {
+  for (const testPath of tests) {
+    try {
+      const goldenPath = testPath + '.golden';
+      const input = Deno.readTextFileSync(testPath);
+      const output = prettyPrint(parseProgram(input));
+      const diffCommand = new Deno.Command('diff', {
+         args: ['-du', goldenPath, '-'],
+         stdin: 'piped',
+         stdout: 'piped',
+         stderr: 'piped'
+      });
+      const child = diffCommand.spawn();
+      const writer = child.stdin.getWriter();
+      writer.write(new TextEncoder().encode(output));
+      writer.releaseLock();
+      await child.stdin.close();
+      const result = await child.output();
+      const stdout = new TextDecoder().decode(result.stdout);
+      const stderr = new TextDecoder().decode(result.stderr);
+      if (!stdout && !stderr) {
+        console.log(`${testPath} pass`);
+        continue;
+      }
+      if (stderr) {
+        console.log(`${testPath} missing golden`);
+        console.log(output);
+      } else if (stdout) {
+        console.log(`${testPath} diff`);
+        console.log(stdout);
+      }
+      if (confirm('gild?')) {
+        Deno.writeTextFileSync(goldenPath, output);
+      }
+    } catch (err: string) {
+      console.log(`${testPath} errors\n${err}`);
+    }
+  }
+}
+
+runTests(Deno.args);
