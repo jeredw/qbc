5a53eaf40de54d8e49260310fcfde9b4d96fd0a0
Jered Wierzbicki <jeredw@gmail.com>
2025-02-10 06:29:24 -0800

    weird vsc missing change

--- src/statements/Asc.ts
+++ src/statements/Asc.ts
@@ -1,14 +1,12 @@
-import { Token } from "antlr4ng";
-import { ExprContext } from "../../build/QBasicParser.ts";
 import { RuntimeError } from "../Errors.ts";
 import { ILLEGAL_FUNCTION_CALL, integer, isString, Value } from "../Values.ts";
-import { Variable } from "../Variables.ts";
 import { charToAscii } from "../AsciiChart.ts";
 import { BuiltinFunction1 } from "./BuiltinFunction.ts";
+import { BuiltinParams } from "../Builtins.ts";
 
 export class AscFunction extends BuiltinFunction1 {
-  constructor(token: Token, params: ExprContext[], result?: Variable) {
-    super(token, params, result);
+  constructor(params: BuiltinParams) {
+    super(params);
   }
 
   override calculate(input: Value): Value {
