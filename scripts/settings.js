8542f3efb6130ae3f12aef4975eb2c9e001068ac
Jered Wierzbicki <jeredw@gmail.com>
2025-08-10 15:01:09 -0700

    website stuff

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,23 @@
 import esbuildPluginTsc from 'esbuild-plugin-tsc';
+import fs from 'fs';
+
+const renamePlugin = () => ({
+  name: 'rename-plugin',
+  setup(build) {
+    build.onEnd(async () => {
+      try {
+        fs.renameSync('./www/Shell.js', './www/shell.js');
+        fs.renameSync('./www/Shell.js.map', './www/shell.js.map');
+        fs.renameSync('./www/Shell.css', './www/shell.css');
+        fs.renameSync('./www/Shell.css.map', './www/shell.css.map');
+        fs.renameSync('./www/catalog/Catalog.js', './www/catalog/catalog.js');
+        fs.renameSync('./www/catalog/Catalog.js.map', './www/catalog/catalog.js.map');
+      } catch (e) {
+        console.error('Failed to rename file:', e);
+      }
+    });
+  },
+});
 
 export function createBuildSettings(options) {
   return {
@@ -12,6 +31,7 @@ export function createBuildSettings(options) {
       esbuildPluginTsc({
         force: true
       }),
+      renamePlugin(),
     ],
     ...options
   };
